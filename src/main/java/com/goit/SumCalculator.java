787b8417a3ea43144f9ec72357f0b6558dac128b
Svyatoslav Shypka <svyat200909@gmail.com>
2023-04-06 17:26:51 +0200

    Tests_final refactored IntStream

--- src/main/java/com/goit/SumCalculator.java
+++ src/main/java/com/goit/SumCalculator.java
@@ -1,14 +1,19 @@
 package com.goit;
 
 
+import java.util.stream.IntStream;
+
 public class SumCalculator {
 
     int sum(int n){
         if (n <= 0) throw new IllegalArgumentException("invalid data");
+/*
         int suma = 0;
         for (int i = 1; i <= n ; i++) {
             suma = suma + i;
         }
         return suma;
+*/
+        return IntStream.rangeClosed(0, n).sum();
     }
 }
